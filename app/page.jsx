e0694c5ce7ff2a5f2874202b17466c8f11e1874e
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-05-06 14:49:52 +0900

    fix: Home 화살표 함수로 변경

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
-export default function Home() {
+const Home = () => {
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold">Next.js 타임어택을 시작해보세요</h1>
     </main>
   );
-}
+};
+
+export default Home;
