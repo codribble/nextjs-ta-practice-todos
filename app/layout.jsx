6451bd6ae003c9bb85f0779c7ee99a707e17c9bc
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-05-06 14:48:24 +0900

    fix: 구글 폰트 East Sea Dokdo 추가 & 공통 컴포넌트 Header, Footer 연결

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,14 @@
 import "./globals.css";
+import { East_Sea_Dokdo } from "next/font/google";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const dokdo = East_Sea_Dokdo({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dokdo",
+});
 
 export const metadata = {
   title: "Todo App",
@@ -7,8 +17,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko">
-      <body className="min-h-screen">{children}</body>
+    <html lang="ko" className={dokdo.variable}>
+      <body className="min-h-screen text-xl font-[family-name:var(--font-dokdo)]">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
