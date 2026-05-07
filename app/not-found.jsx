52a6dda339229e9ca594032806f7ea329fcd1b1d
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-05-07 17:36:28 +0900

    feat: not found 페이지 추가

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center py-10 text-4xl">
+      404 Not Found!
+    </div>
+  );
+};
+
+export default NotFound;
